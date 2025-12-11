6e02fbcf6dce7de302d89c4ec3c21e700788b0a2
Mohammed Magdy <ahmed12s3462@gmail.com>
2025-12-11 21:27:25 +0200

    Add .gitignore to protect env files

---
+++ lib/presentation/pages/splash/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SplachScreen extends StatelessWidget {
+  const SplachScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Splash Screen',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
